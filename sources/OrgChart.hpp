6d903b9cbdd850e9f62c99bd79de2edaa83649de
Guppi4 <kirill.perevalov.1996@gmail.com>
2022-05-22 19:54:18 +0300

    iterrator

--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -8,9 +8,9 @@ using namespace std;
 namespace ariel
 {
     // flags for iterators
-    const unsigned int inorder_flag = 0;
+    const unsigned int level_order_flag = 0;
     const unsigned int preorder_flag = 1;
-    const unsigned int postorder_flag = 2;
+    const unsigned int level_order_reverse_flag = 2;
 
     class OrgChart
     {
@@ -45,5 +45,45 @@ namespace ariel
         TNode *getroot();
         void printNTree();
         void printNTree_help(TNode *x, vector<bool> flag, int depth, bool isLast);
+           class iterator{
+                private:
+		            TNode* pointer_to_node;
+                    queue<TNode*> tq;
+
+                public:  
+                    iterator(unsigned int flag,TNode* ptr): pointer_to_node(ptr){
+                        if(pointer_to_node != nullptr){
+                            tq.push(nullptr);
+                            if(flag == level_order_flag){level_order(pointer_to_node);}
+                            if(flag == preorder_flag){Preoder(pointer_to_node);}
+                            if(flag == level_order_reverse_flag){level_order_reverse(pointer_to_node);}
+                           
+                        }
+   
+                    }
+                    iterator(TNode* ptr):pointer_to_node(ptr){}  
+                    
+                    string  operator*() const {return pointer_to_node->value;}
+		            string *operator->() const {return &(pointer_to_node->value);}
+                    void level_order(TNode* root);
+                    void Preoder(TNode* root);
+                    void level_order_reverse(TNode* root);
+                    
+		            iterator& operator++() {
+                        tq.pop();
+			            pointer_to_node = tq.front();
+			            return *this;
+		            }
+		            iterator operator++(int) {
+			            iterator tmp = *this;
+                        tq.pop();
+			            pointer_to_node = tq.front();
+			            return tmp;
+		            }
+
+		            bool operator==(const iterator& rhs) const {return pointer_to_node == rhs.pointer_to_node;}
+		            bool operator!=(const iterator& rhs) const {return pointer_to_node != rhs.pointer_to_node;}
+            };
+    
     };
 }
